b23bd498ba096c42f97a903d5a1b229c9330fe89
SaranSelvaraj <saranpvsselvaraj@gmail.com>
2025-03-09 01:20:49 -0600

    Delete My protfolio directory

--- My protfolio/scripts.js
+++
@@ -1,5 +0,0 @@
-document.getElementById('contact-form').addEventListener('submit', function(e) {
-    e.preventDefault();
-    alert('Thank you for your message!');
-    // Here you can add code to handle form submission, e.g., sending data to a server
-});
